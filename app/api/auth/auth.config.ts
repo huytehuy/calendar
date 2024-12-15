789e673d30f18aa7eabc517c59fb86c97b9e3a4a
Pham Khanh Huy <phamkhanhhuy1231@gmail.com>
2024-12-15 23:54:11 +0700

    Add NextAuth secret configuration

--- app/api/auth/auth.config.ts
+++ app/api/auth/auth.config.ts
@@ -2,7 +2,12 @@ import { AuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import { JWT } from 'next-auth/jwt'
 
+if (!process.env.NEXTAUTH_SECRET) {
+  throw new Error('Please provide NEXTAUTH_SECRET environment variable')
+}
+
 export const authOptions: AuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || '',
